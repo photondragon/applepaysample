cf009acbf54d641ed08e7ed63c3c86eec9d94bb9
photondragon <photondragon@163.com>
2016-04-30 22:47:26 +0800

    Initial Commit

---
+++ applepaysample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  applepaysample
+//
+//  Created by photondragon on 16/4/30.
+//  Copyright © 2016年 mahu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
